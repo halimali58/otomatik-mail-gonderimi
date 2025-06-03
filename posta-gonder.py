9537445e17c16b8f721da3236659de832a9e9188
HaLiM_58 <128403451+halimali58@users.noreply.github.com>
2025-06-03 22:12:52 +0300

    Update posta-gonder.py

--- posta-gonder.py
+++ posta-gonder.py
@@ -437,7 +437,7 @@ def send_email(excel_file_name):
         print(f"✅ {excel_file_name} dosyası {RECIPIENT_EMAIL} adresine başarıyla gönderildi.")
     except Exception as e:
         print(f"⚠️ E-posta gönderilirken hata: {e}")
-        raise  # Hatanın loglara yazılmasını sağla
+        raise  # Hatanın log TLD'ye yazılmasını sağla
 
 # Excel dosyası indirme bağlantısı
 def provide_download_link(excel_file_name):
